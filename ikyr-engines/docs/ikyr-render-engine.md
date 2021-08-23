d062b046518ac2f38c0e219bef70c6fa402eef0b
Kryx-Ikyr <kryx.mail@gmail.com>
2021-08-23 17:12:22 +0200

    [ADD] docausorus tests

---
+++ ikyr-engines/docs/ikyr-render-engine.md
@@ -0,0 +1,20 @@
+---
+sidebar_position: 2
+---
+# Ikyr Render Engine
+
+## Overview
+
+By using Vulkan, Ikyr Render Engine will help you build your graphical 2D-3D application.
+
+## Installation
+
+    TODO
+
+## Basic usage
+
+    TODO
+
+## API
+
+    TODO
---
+++ ikyr-engines/docusaurus.config.js
@@ -0,0 +1,134 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Ikyr Engines',
+  tagline: 'Use the right engine for your abstract level',
+  url: 'https://Kryx-Ikyr.github.io',
+  baseUrl: '/ikyr-engines/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Kryx-Ikyr', // Usually your GitHub org/user name.
+  projectName: 'ikyr-engines-API', // Usually your repo name.
+  trailingSlash: false,
+  themeConfig: {
+    navbar: {
+      title: 'Ikyr',
+      logo: {
+        alt: 'Ikyr Engines Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          type: 'doc',
+          docId: 'ikyr-render-engine',
+          position: 'left',
+          label: 'Render Engine',
+        },
+        {
+          type: 'doc',
+          docId: 'ikyr-game-engine',
+          position: 'left',
+          label: 'Game Engine',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+  },
+};
